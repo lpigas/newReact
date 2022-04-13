c4c5f16596b27cecab68b2192ba638b775717c1b
lpigas <igornixdn@gmail.com>
2022-04-13 16:00:21 +0300

    all work add second sort add reset, css properties

--- react/app/src/Components/Funct.js
+++ react/app/src/Components/Funct.js
@@ -1,35 +1,14 @@
-import React, { useState } from 'react'
-// import Ccomponents from './Ccomponents'
 
 
-
-    
-
-    function Funct(){
-        
-        const [alt, setAlt] = useState([])
-        const fec = () =>{
-            const newArr = [];
-            fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-            .then(res => res.json())
-            .then(x => x.drinks.map((item)=> newArr.push(item)))
-            .then(y => setAlt(newArr))
-    
-            
-        }
-        console.log(alt)
-        return(
-            <ul>
-                <button onClick={fec}>Loading</button>
-          {alt.map((item, index) => {
-           return (
-           <li key={index}>
-             {item.strDrink}
-              <img border="2px solid black" width='50' height='50' alt={item.name} src={item.strDrinkThumb}></img>
-
-            </li>)
-          })}
-        </ul>
-        )
-    }
-    export default Funct
+const FunctionSortFirst =() => {     
+  if( items % 2 === 0) {
+    drinks.sort((a, b) => {
+      return (a.strDrink.localeCompare(b.strDrink))})
+    } else {
+      drinks.sort((a, b) => {
+        return (b.strDrink.localeCompare(a.strDrink))})
+      }
+  setTest(true)
+  return (drinks);
+}
+    export default FunctionSortFirst
--- react/app/src/Components/Functions.js
+++ react/app/src/Components/Functions.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
+
 const styles = {
-  // display:'flex',
-  border:'3px solid black',
-  color:'white',
+  border:'3px solid white',
+  color: "white",
   margin: '15px',
   justifyContent: 'center',
   width:'175%'
@@ -12,6 +12,7 @@ export default function Functions() {
     const [drinks,setDrinks] = useState([]);
     const [isLoaded, setIsLoading] = useState(false)
     const [test, setTest] = useState(false)
+    const [testSecond, setTestSecond] = useState(false)
     const [items, setItems] = useState(1)
     const getData = async () =>{
         setIsLoading(true);
@@ -20,52 +21,89 @@ export default function Functions() {
         setDrinks(data.drinks);
         setIsLoading(false);
         setIsLoading(false)
-    }
-    useEffect(() =>{
+      }
+      useEffect(() =>{
         getData()
-    },[])
-    useEffect(() =>{
-      
+      },[])
+      useEffect(() =>{
+        
+        
     },[drinks])
 
     
     
-    const Functionsort =() => {
+    const FunctionSortFirst =() => {     
       if( items % 2 === 0) {
+        document.querySelector('.first').classList.add('activeS')
+        document.querySelector('.first').classList.remove('activeF')
+        document.querySelector('.second').classList.remove('activeF', 'activeS')
         drinks.sort((a, b) => {
           return (a.strDrink.localeCompare(b.strDrink))})
         } else {
+          document.querySelector('.first').classList.add('activeF')
+          document.querySelector('.first').classList.remove('activeS')
+          document.querySelector('.second').classList.remove('activeF', 'activeS')
           drinks.sort((a, b) => {
             return (b.strDrink.localeCompare(a.strDrink))})
           }
       setTest(true)
       return (drinks);
     }
+    
+    const FunctionSortSecond =() => {     
+      if( items % 2 === 0) {
+        drinks.sort((a, b) => {
+          document.querySelector('.second').classList.add('activeS')
+          document.querySelector('.second').classList.remove('activeF')
+          document.querySelector('.first').classList.remove('activeF', 'activeS')
+          return (a.idDrink - b.idDrink)})
+        } else {
+          drinks.sort((a, b) => {
+            document.querySelector('.second').classList.add('activeF')
+            document.querySelector('.second').classList.remove('activeS')
+            document.querySelector('.first').classList.remove('activeF', 'activeS')
+            return (b.idDrink -a.idDrink)})
+          }
+      setTestSecond(true)
+      return (drinks);
+    }
+    const FunctionReset = () =>{
+      getData()
+      document.querySelector('.second').classList.remove('activeS', 'activeF')
+      document.querySelector('.first').classList.remove('activeF', 'activeS')
+    }
 
     useEffect(() =>{
-      if(test){
-        setDrinks(Functionsort())
+      if(test === true && testSecond === false){
+        setDrinks(FunctionSortFirst())
         setItems(Math.floor((items+1)))
         setTest(false)
+    } else if(test === false && testSecond === true) {
+        setDrinks(FunctionSortSecond())
+        setItems(Math.floor((items+1)))
+        setTestSecond(false)
     }
-  })
 
+  })
 
     
 
     
     return (
-    <div>
-      {isLoaded && <p>Loading,,,</p>}
+      <div>
+      {isLoaded && <p style={{color:'white'}}>Loading,,,</p>}
       <table className='drinks' id='Table'>
         <tbody>
-        <tr className='appx'>
-          <td className='appx' onClick={Functionsort} >Напитки</td>
-          <td className='appx' onClick={Functionsort}>Состав</td>
+      <button onClick={FunctionReset}>reset</button>
+        <tr className='appx' style={{color:'white'}}>
+          <td className='appx first' onClick={FunctionSortFirst} >Напитки</td>
+          <td className='appx second' onClick={FunctionSortSecond}>Код</td>
+          <td className='appx' >Фото</td>
         </tr>
         {drinks.map(i => <tr key={i.idDrink}>
           <td style={styles}>{i.strDrink}</td> 
           <td style={styles} color="red">{i.idDrink}</td>
+          <td style={styles} color="red"><img height='50px' width='50px' src={i.strDrinkThumb} alt={i.idDrink} /></td>
         </tr>
          ) }
         
